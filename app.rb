bb92b09d8567a0614f4eb7dd72e1979774c72e8b
Chris <cm1099769+ macagare@googlemail.com>
2014-10-11 16:08:09 +0200

    upload snapshot, display snapshots as grid

--- app.rb
+++ app.rb
@@ -1,7 +1,27 @@
 require "sinatra"
+require "json"
 
 set :public_folder, 'public'	
 
 get "/" do
   redirect '/index.html'
+end
+
+post "/upload/:id" do |id|
+	if id.to_s.length
+		File.open('public/images/' + "#{params[:id]}.jpg", "wb") do |f|
+		  f.write(params['webcam'][:tempfile].read)
+		end 
+	end
+	return "success"
+end
+
+get "/images" do
+	puts "images called"
+	puts params.inspect
+	files = Array.new
+	Dir.new("public/images").each do |file|
+		files << file unless File.directory?(file)
+	end
+	return files.to_json
 end
--- public/js/main.js
+++ public/js/main.js
@@ -1,46 +1,62 @@
 app = (function(win, doc, undefined) {
 	var timer
 		, webcamEnabled = false
-		, SNAP_TIMEOUT = 3000
+		, SNAP_TIMEOUT = 2000
 		, takeSnapshot = function() {
-			console.log("takeSnapshot");
-			if (!webcamEnabled) {
-	  			Webcam.on("live", function() {
-	  				setTimeout(function() {
-	  					webcamEnabled = true;
-	  					createSnapshot();
-	  					turnOffCamera();
-	  				}, SNAP_TIMEOUT);
-	  			});
-	  			Webcam.on("error", function(error) {
-	  				alert(error);
-	  			});
-	  			startCapture();
-	  		} else {
-	  			Webcam.snap( setPreview );
-	  		}
+			setTimeout(function(){
+				Webcam.snap( setPreview );
+			}, SNAP_TIMEOUT);
     	}
-    	, createSnapshot = function() {
-    		Webcam.snap( setPreview );
-    	}
-    	, startCapture = function(){
+    	, startWebcamStream = function(){
+            Webcam.on("live", function() {
+                webcamEnabled = true;
+            });
+            Webcam.on("error", function(error) {
+                alert(error);
+            })
     		Webcam.attach( '#my_camera' );
     	}
     	, setPreview = function(data_uri) {
     		$("#my_result").html('<img src="'+data_uri+'"/>');
+    		upload( data_uri );
     	}
-    	, turnOffCamera = function() {
+    	, stopWebcamStream = function() {
     		webcamEnabled = false;
     		Webcam.reset();
-    	};
+    	}
+    	, freezeStream = function() {
+    		Webcam.freeze();
+    	}, upload = function(data_uri) {
+    		Webcam.upload( data_uri, '/upload/123', function(code, text) {
+		        console.log(code, text);
+		    } );
+    	}
+        , refreshUserImages = function() {
+            $.get( "/images", function( data ) {
+                deleteUserImages();
+                addUserImages(data);
+            }, "json" );
+        }
+        , deleteUserImages = function() {
+            $("#imageGrid .snapshot").remove();
+        }
+        , addUserImages = function(filenames) {
+            for (var i = filenames.length - 1; i >= 0; i--) {
+                $( "#imageGrid" ).append( '<div class="snapshot"><img src="images/' + filenames[i] + '"></div>' );
+            };
+        };
 
 	$(doc).ready(function(){
-		
+		startWebcamStream();
+        refreshUserImages();
 	});
 
 	return {
 		takePhoto: function() {
 			takeSnapshot();
-		}
+		},
+        reloadImages:function() {
+            refreshUserImages();
+        }
 	};
 })(window, document);
